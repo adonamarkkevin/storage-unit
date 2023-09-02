271c7445ec0b8a4bf73af5e60c0a098266826161
Mark Kevin Adona <adonamarkkevin@gmail.com>
2023-09-02 13:51:44 +0800

    feat: db connect

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,14 @@ AppDataSource.initialize()
 
         app.use(express.json());
         app.use(cors(corsOpts));
+
+        const PORT = 8181;
+
+        app.listen(PORT, () => {
+            console.log(`server started on localhost: ${PORT}`);
+        });
     })
-    .catch((error) => console.log(error));
+    .catch((error) => {
+        console.log("TypeORM connection error: ", error);
+        process.exit(0);
+    });
